83b3d94f0147d06e2699775e610dd48c1a3b07db
CaiEddie <>
2019-06-07 16:37:35 -0400

    Update HistQ.py

--- HistQ.py
+++ HistQ.py
@@ -39,6 +39,8 @@ def getChannels(subFolder):
 
 	gd.addStringField("Brain Region:", "")
 
+	gd.addStringField("Minimum size:", "24")
+
 	gd.addMessage("Name the markers associated with this directory:")
 	gd.addMessage(inputDirectory + subFolder)  
 	gd.addMessage("(Leave empty to ignore)")
@@ -52,6 +54,8 @@ def getChannels(subFolder):
 	channelNames = []
 
 	region = gd.getNextString()
+
+	tooSmall = gd.getNextString()
   	
   	channelNames.append([gd.getNextString(), 0])
   	channelNames.append([gd.getNextString(), 1])
@@ -66,7 +70,7 @@ def getChannels(subFolder):
 		print "User canceled dialog!"  
 		return
 		
-  	return region, channels
+  	return region, tooSmall, channels
 
 # Function to get the thresholds.
 
@@ -322,6 +326,8 @@ def process(subFolder, outputDirectory, filename):
 
 		summary[v+"-particles/tissue-area"] = blobsnuclei[x] / bigareas[0]
 		fieldnames.append(v+"-particles/tissue-area")	
+
+		fieldnames.append(v+"-positive/tissue-area")
 	  	
 	# Adds the column for colocalization between first and second marker
   
@@ -388,15 +394,20 @@ def process(subFolder, outputDirectory, filename):
 
 	# Calculate the average of the particles sizes
 
+	for chan in channels:
+  		v, x = chan
+	  	summary[v+"-positive/tissue-area"] = summary[v+"-positive"] / bigareas[0]
+	  	
+
   	if float(summary['#nuclei']) > 0: 
 			summary['size-average'] = round( areaCounter / summary['#nuclei'], 2)
 
 	# Opens and appends one line on the final csv file for the subfolder (remember that this is still inside the loop that goes through each image)
 
-	with open(outputDirectory + "/output.csv", 'a') as csvfile:		
+	with open(outputDirectory + "output_" + time +".csv", 'a') as csvfile:		
 	
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore', lineterminator = '\n')
-		if os.path.getsize(outputDirectory + "/output.csv") < 1:
+		if os.path.getsize(outputDirectory + "output_" + time +".csv") < 1:
 			writer.writeheader()
 		writer.writerow(summary)
 
@@ -413,9 +424,11 @@ outputDirectory = dc.getDirectory()
 
 # Opens log file
 
+time = datetime.datetime.now().strftime("%Y-%m-%d")
+
 with open(outputDirectory + "log.txt", "w") as log:
 	
-	log.write("log: "+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+	log.write("log: "+ time)
 	log.write("\n")
 	
 	log.write("________________________\n")
@@ -458,11 +471,12 @@ with open(outputDirectory + "log.txt", "w") as log:
 	
 	allChannels = []
 	allRegions = []
+	allSmalls = []
 	for subFolder in directories:
-		region, chan = getChannels(subFolder)
+		region, tooSmall, chan = getChannels(subFolder)
 		allChannels.append(chan)
 		allRegions.append(region)
-
+		allSmalls.append(tooSmall)
 
 	
 	# Loop that goes through each sub folder. 
@@ -471,7 +485,7 @@ with open(outputDirectory + "log.txt", "w") as log:
 	log.write("Beginning main directory loop: \n")
 	log.write("\n")
 
-	open(outputDirectory + "/output.csv", 'w').close
+	open(outputDirectory + "output_" + time +".csv", 'w').close
 	
 	for inde, subFolder in enumerate(directories):
 	
@@ -481,11 +495,11 @@ with open(outputDirectory + "log.txt", "w") as log:
 		
 		channels = allChannels[inde]
 		region = allRegions[inde]
-
+		tooSmallThreshold = int(allSmalls[inde])
 	
 		log.write("Channels: "+ str(channels) +"\n")
 		
-		lowerBounds = [209, 180, 205, 205, 205]
+		lowerBounds = [205, 180, 205, 205, 205]
 		for chan in channels:
 			v, x = chan
 			if (v + '-' + region) in thresholds:
