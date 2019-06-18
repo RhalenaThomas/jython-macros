7ed9f0dd102e86158dd7e6e6d2dadfd8410c5f1c
CaiEddie <>
2019-06-18 08:55:09 -0400

    remove empty space

--- cellQ_combine.py
+++ cellQ_combine.py
@@ -8,13 +8,27 @@ outputName = "combined.csv"
 
 fields = []
 
+d1,d2,d3 = "zzz"
+
 files = os.listdir(directory)
 
 for filename in files: 
 	if "output" in filename:
-	    with open(directory + '/' + filename) as csvfile:
+		with open(directory + '/' + filename) as csvfile:
 			reader = csv.DictReader(csvfile)
-			for name in reader.fieldnames:
+			for i, name in enumerate(reader.fieldnames):
+
+				if i == 17:
+					d1 = name.replace("-positive", "")
+				if i == 22:
+					d2 = name.replace("-positive", "")
+				if i == 26:
+					d3 = name.replace("-positive", "")
+
+				name = name.replace(d1, "Ch1")
+				name = name.replace(d2, "Ch2")
+				name = name.replace(d3, "Ch3")
+
 				if name not in fields:
 					fields.append(name)
 
