4b7a01fba409931ed7d81084cbd9edb3edbf8c3f
Rhalena Thomas - T. Durcan Lab <rthomas@bic.mni.mcgill.ca>
2019-06-18 11:46:02 -0400

    add scores

--- cellQ_combine.py
+++ cellQ_combine.py
@@ -6,11 +6,16 @@ directory = tkFileDialog.askdirectory(parent=root,initialdir="/",title='Please s
 outputName = "combined.csv"
 
 
-fields = ["Ch1", "Ch2", "Ch3"]
+fields = ["Ch1", "Ch2", "Ch3", "Media", "Score"]
 
 channels = {}
 
 d1,d2,d3 = "zzz"
+
+med = ""
+
+scores = {"mTeSR1":[6, 3.75, 6.5, 4, 3.5, 6.5, 4.25, 5.25, 4.5, 5.25, 0.75, 2.5],"E8":[3.75, 4.75, 5, 4.25, 4.5, 6, 5.5, 5, 4.25, 4.5, 3, 4]}
+
 
 files = os.listdir(directory)
 
@@ -34,7 +39,12 @@ for filename in files:
 				if name not in fields:
 					fields.append(name)
 
-		channels[filename] = [d1, d2, d3]
+		if "E8" in filename:
+			med = "E8"
+		if "mTeSR" in filename:
+			med = "mTeSR1"
+
+		channels[filename] = [d1, d2, d3, med]
 
 
 with open(directory + '/' + outputName, 'w') as csvfile:		
@@ -48,9 +58,13 @@ with open(directory + '/' + outputName, 'w') as csvfile:
 				for i, row in enumerate(reader):
 
 					if i != 0:
-						row.insert(0,channels[filename][0])
-						row.insert(1,channels[filename][1])
-						row.insert(2,channels[filename][2])
+						
+						row.insert(0, channels[filename][0])
+						row.insert(1, channels[filename][1])
+						row.insert(2, channels[filename][2])
+						row.insert(3, channels[filename][3])
+						row.insert(4, scores[channels[filename][3]][int(row[9])-1])
+
 
 						writer.writerow(row)
 
