961a45e135cc940416106039bdd21557a270a62b
CaiEddie <>
2019-06-19 09:30:45 -0400

    add more scores and per nuclei

--- cellQ_combine.py
+++ cellQ_combine.py
@@ -6,14 +6,18 @@ directory = tkFileDialog.askdirectory(parent=root,initialdir="/",title='Please s
 outputName = "combined.csv"
 
 
-fields = ["Ch1", "Ch2", "Ch3", "Media", "Score"]
+fields = ["Ch1", "Ch2", "Ch3", "Media", "Growth", "Viability", "Differentiation", "Attachment", "Score"]
 
-channels = {}
+info = {}
 
-d1,d2,d3 = "zzz"
+d1,d2,d3 = ""
 
 med = ""
 
+growths = {"mTeSR1":[1.5, 0, 2.25, 0.75, 0.5, 1.5, 0.5, 2, 1.5, 2, 0.5, 1], "E8":[0.5, 1.75, 1.25, 1.75, 1.75, 2, 2, 1.75, 1.5, 1.75, 1.5, 1.5]}
+viabilities = {"mTeSR1":[1.75, 1.5, 1.5, 1.75, 1.5, 2, 1.25, 1.5, 1.75, 1.75, 1. 1,5], "E8":[1.5, 1.25, 1.75, 1.5, 1.75, 1.5, 1.5, 1.75, 1.5, 1.75, 1.5, 1.5]}
+differentiations = {"mTeSR1":[2.5, 2.5, 2.5, 2, 2, 2.5, 2.25, 2, 1.5, 1.75, 0, 0.5], "E8":[2.5, 2.5, 2.25, 2,2,2.5, 2.25, 2.25, 1.5, 1.75, 1.75, 1.5]}
+attachments = {"mTeSR1":[2.25, 1.75, 2.25, 1.5, 1.5, 2.5, 2.25, 1.75, 1.75, 1.75, 1.75, 1.25, 1.5], "E8":[1.25, 1.25, 1.75, 1, 1, 2, 1.75, 1.25, 1.25, 1.25, 0, 1.25]}
 scores = {"mTeSR1":[6, 3.75, 6.5, 4, 3.5, 6.5, 4.25, 5.25, 4.5, 5.25, 0.75, 2.5],"E8":[3.75, 4.75, 5, 4.25, 4.5, 6, 5.5, 5, 4.25, 4.5, 3, 4]}
 
 
@@ -44,27 +48,42 @@ for filename in files:
 		if "mTeSR" in filename:
 			med = "mTeSR1"
 
-		channels[filename] = [d1, d2, d3, med]
-
+		info[filename] = [d1, d2, d3, med]
 
 with open(directory + '/' + outputName, 'w') as csvfile:		
 	writer = csv.writer(csvfile)
 	writer.writerow(fields)
 	for filename in files:
 		if "output" in filename:
+
+			m = info[filename][3]
+			
 			with open(directory + '/' + filename) as csvfile:
 				reader = csv.reader(csvfile)
 
 				for i, row in enumerate(reader):
 
-					if i != 0:
+					if i == 0:
+						for j in range(6, len(fields)-1):
+							row.append(row[j] + "_per_nuclei")
+
+					else:
 						
+						for j in range(6, len(fields)-1):
+							row.append(row[j]/row[10])
+
 						row.insert(0, channels[filename][0])
 						row.insert(1, channels[filename][1])
 						row.insert(2, channels[filename][2])
 						row.insert(3, channels[filename][3])
-						row.insert(4, scores[channels[filename][3]][int(row[9])-1])
+						
+						line = int(row[9])
 
+						row.insert(4, growths[m][line-1])
+						row.insert(5, viabilities[m][line-1])
+						row.insert(6, differentiations[m][line-1])
+						row.insert(7, attachments[m][line-1])
+						row.insert(8, scores[m][line-1])
 
 						writer.writerow(row)
 
