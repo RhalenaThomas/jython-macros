8b763ad75a8cd2e3901f1f35bcfcdbd801228635
Rhalena Thomas - T. Durcan Lab <rthomas@bic.mni.mcgill.ca>
2019-10-08 14:28:04 -0400

    close first montage fix final montage

--- stack_to_montage.py
+++ stack_to_montage.py
@@ -24,29 +24,31 @@ for image in os.listdir(inputDirectory):
 	if "stack" in image:
 		imp = IJ.openImage(inputDirectory + "/" + image)
 		imp2 = IJ.openImage(inputDirectory + "/" + image.replace("stack", "montage"))
-		imp2.show()
 		imp.show()
+		imp2.show()
+		
 		
 		gd = GenericDialog("?")
 		gd.addChoice("Would you like to adjust this one?", ["No", "Yes"], "No")
 		gd.showDialog()
 		if gd.getNextChoice() == "Yes":
+			imp2.close();
+			IJ.run("Brightness/Contrast...");
 			WaitForUserDialog("Title", "Adjust intensities").show()
 			IJ.run("Stack to Images", "");
 			IJ.run(imp, "Merge Channels...", "c2=Green c3=Blue c6=Magenta c7=Yellow create keep");
 			imp = WindowManager.getCurrentImage() 
 			IJ.run(imp, "RGB Color", "");
-		
 			IJ.run(imp, "Images to Stack", "name=Stack title=[] use")
+			#WaitForUserDialog("Title", "Now you should have a stack check it out ").show()
 			imp = WindowManager.getCurrentImage() # the Stack
 
-		imp.show()
-		IJ.setForegroundColor(255, 255, 255)
-		IJ.run(imp, "Make Montage...", "columns=5 rows=1 scale=0.5 borderWidth = 2 useForegroundColor = True")
-		
-		IJ.run("Save", "save=" + outputDirectory + '/' + image.replace("stack", "newmontage"))
+			imp.show()
+			IJ.setForegroundColor(255, 255, 255)
+			IJ.run(imp, "Make Montage...", "columns=5 rows=1 scale=0.5 borderWidth = 2 useForegroundColor = True")
+			#WaitForUserDialog("Title", "Now we should have the montage").show()
+			IJ.run("Save", "save=" + outputDirectory + '/' + image.replace("stack", "newmontage"))
 
 		IJ.run("Close All", "")
 
 print("Done!")
-
